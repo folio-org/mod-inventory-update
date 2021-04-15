345f358e3a8b1d7093c9a4cbfca41149878da68c
Niels Erik G. Nielsen <nielserik@indexdata.com>
2021-04-15 17:13:23 +0200

    HRID API: return 422 when Instance HRID is missing

--- src/test/java/org/folio/inventoryupdate/test/InventoryUpdateTestSuite.java
+++ src/test/java/org/folio/inventoryupdate/test/InventoryUpdateTestSuite.java
@@ -1310,6 +1310,22 @@ public class InventoryUpdateTestSuite {
    }
 
   @Test
+  public void upsertByHridWithMissingInstanceHridWillBeRejected (TestContext testContext) {
+    Response upsertResponse = upsertByHrid(422, new JsonObject()
+            .put("instance",
+                    new InputInstance().setTitle("Initial InputInstance").setInstanceTypeId("12345").getJson())
+            .put("holdingsRecords", new JsonArray()
+                    .add(new InputHoldingsRecord().setHrid("HOL-001").setPermanentLocationId(LOCATION_ID_1).setCallNumber("test-cn-1").getJson()
+                            .put("items", new JsonArray()
+                                    .add(new InputItem().setBarcode("BC-001").getJson())
+                                    .add(new InputItem().setHrid("ITM-002").setBarcode("BC-002").getJson())))
+                    .add(new InputHoldingsRecord().setHrid("HOL-002").setPermanentLocationId(LOCATION_ID_1).setCallNumber("test-cn-2").getJson()
+                            .put("items", new JsonArray()
+                                    .add(new InputItem().setBarcode("BC-003").getJson())))));
+  }
+
+
+  @Test
   public void upsertByHridWithMissingItemHridWillBeRejected (TestContext testContext) {
     String instanceHrid = "1";
     Response upsertResponse = upsertByHrid(422, new JsonObject()

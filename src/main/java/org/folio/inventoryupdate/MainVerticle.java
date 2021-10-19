bff17187ec8d00e3e73a2e879b51d1de30c28ffb
Niels Erik <nielserik@indexdata.com>
2021-10-19 09:47:42 +0200

    Add health check endpoint (MODINVUP-12) (#33)

--- src/main/java/org/folio/inventoryupdate/MainVerticle.java
+++ src/main/java/org/folio/inventoryupdate/MainVerticle.java
@@ -12,6 +12,7 @@ import io.vertx.ext.web.handler.StaticHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
+  public final static String HEALTH_CHECK = "/admin/health";
   public final static String INVENTORY_UPSERT_HRID_PATH = "/inventory-upsert-hrid";
   public final static String SHARED_INVENTORY_UPSERT_MATCHKEY_PATH = "/shared-inventory-upsert-matchkey";
 
@@ -38,6 +39,7 @@ public class MainVerticle extends AbstractVerticle {
     router.delete(INVENTORY_UPSERT_HRID_PATH).handler(upsertService::handleInventoryRecordSetDeleteByHRID);
     router.delete(SHARED_INVENTORY_UPSERT_MATCHKEY_PATH).handler(upsertService::handleSharedInventoryRecordSetDeleteByIdentifiers);
     router.route("/apidocs/*").handler(StaticHandler.create("apidocs"));
+    router.route(HEALTH_CHECK).handler(upsertService::handleHealthCheck);
     router.route("/*").handler(upsertService::handleUnrecognizedPath);
 
     vertx.createHttpServer()

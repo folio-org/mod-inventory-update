c756dc2cb256fbe6cc480a1989b185288aee50f7
nielserik <nielserik@indexdata.com>
2025-11-24 08:31:22 +0100

    MODINVUP-128 MODINVIMP-42 add 'metadata' to records
      - java doc

--- src/main/java/org/folio/inventoryupdate/importing/moduledata/Entity.java
+++ src/main/java/org/folio/inventoryupdate/importing/moduledata/Entity.java
@@ -23,127 +23,144 @@ import java.util.stream.Collectors;
 
 public abstract class Entity {
 
-    public static final Logger logger = LogManager.getLogger("inventory-import");
-    public static final String DATE_FORMAT = "YYYY-MM-DD''T''HH24:MI:SS,MS";
+  public static final Logger logger = LogManager.getLogger("inventory-import");
+  public static final String DATE_FORMAT = "YYYY-MM-DD''T''HH24:MI:SS,MS";
 
 
-    /**
-     * Implement to return an enum identifier for the underlying database table for the implementing entity.
-     * @return a Tables enum value
-     */
-    public abstract Tables table();
+  /**
+   * Implement to return an enum identifier for the underlying database table for the implementing entity.
+   *
+   * @return a Tables enum value
+   */
+  public abstract Tables table();
 
-    protected Metadata metadata = new Metadata();
+  protected Metadata metadata = new Metadata();
 
-    /**
-     * Build and execute the DDL statements for creating the database objects for persisting the entity.
-     * The default implementation is for the simplest possible table with no foreign key constraints, indexes
-     * or special column validations etc. Commonly overridden.
-     *
-     * @param pool the TenantPgPool for executing the DDL
-     * @return Empty future when done
-     */
-    public Future<Void> createDatabase(TenantPgPool pool) {
-        StringBuilder columnsDdl = new StringBuilder();
-        fields().keySet()
-                .forEach(field -> columnsDdl.append(field(field).pgColumn().getColumnDdl()).append(","));
-        columnsDdl.append(metadata.columnsDdl());
-        return executeSqlStatements(pool,
-                "CREATE TABLE IF NOT EXISTS " + pool.getSchema() + "." + table()
-                        + "("
-                        + columnsDdl
-                        + ")")
-                .mapEmpty();
+  /**
+   * Build and execute the DDL statements for creating the database objects for persisting the entity.
+   * The default implementation is for the simplest possible table with no foreign key constraints, indexes
+   * or special column validations etc. Commonly overridden.
+   *
+   * @param pool the TenantPgPool for executing the DDL
+   * @return Empty future when done
+   */
+  public Future<Void> createDatabase(TenantPgPool pool) {
+    StringBuilder columnsDdl = new StringBuilder();
+    fields().keySet()
+        .forEach(field -> columnsDdl.append(field(field).pgColumn().getColumnDdl()).append(","));
+    columnsDdl.append(metadata.columnsDdl());
+    return executeSqlStatements(pool,
+        "CREATE TABLE IF NOT EXISTS " + pool.getSchema() + "." + table()
+            + "("
+            + columnsDdl
+            + ")")
+        .mapEmpty();
+  }
+
+  protected Future<RowSet<Row>> executeSqlStatements(TenantPgPool pool, String... statements) {
+    Future<RowSet<Row>> future = Future.succeededFuture();
+    for (var sql : statements) {
+      future = future.compose(x -> pool.query(sql).execute()
+          .onFailure(e -> logger.error("Failed to execute [{}]: {}", sql, e.getMessage())));
+    }
+    return future;
+  }
+
+  public String table(String schema) {
+    return schema + "." + table().toString();
+  }
+
+
+  /**
+   * Represents a field of an entity, containing JSON property name, database column name and other features of the field.
+   *
+   * @param jsonPropertyName
+   * @param columnName
+   * @param pgType
+   * @param nullable
+   * @param queryable
+   * @param primaryKey
+   */
+  public record Field(String jsonPropertyName, String columnName, PgColumn.Type pgType, boolean nullable,
+                      boolean queryable, boolean primaryKey) {
+    public Field(String jsonPropertyName, String columnName, PgColumn.Type pgType, boolean nullable, boolean queryable) {
+      this(jsonPropertyName, columnName, pgType, nullable, queryable, false);
     }
 
-    protected Future<RowSet<Row>> executeSqlStatements(TenantPgPool pool, String... statements) {
-            Future<RowSet<Row>> future = Future.succeededFuture();
-            for (var sql : statements) {
-                future = future.compose(x -> pool.query(sql).execute()
-                        .onFailure(e -> logger.error("Failed to execute [{}]: {}", sql, e.getMessage())));
-            }
-            return future;
+    public PgColumn pgColumn() {
+      return new PgColumn(columnName, pgType, nullable, primaryKey);
     }
 
-    public String table(String schema) {
-        return schema + "." + table().toString();
+    public String pgColumnDdl() {
+      return pgColumn().getColumnDdl();
     }
+  }
 
+  /**
+   * Implement to provide a map of the {@link Field} fields of the implementing entity
+   *
+   * @return Map fields by field keys to be used for finding queryable fields or, if possible, for creating the database table and more.
+   */
+  public abstract Map<String, Field> fields();
 
-    /**
-     * Represents a field of an entity, containing JSON property name, database column name and other features of the field.
-     * @param jsonPropertyName
-     * @param columnName
-     * @param pgType
-     * @param nullable
-     * @param queryable
-     * @param primaryKey
-     */
-    public record Field(String jsonPropertyName, String columnName, PgColumn.Type pgType, boolean nullable, boolean queryable, boolean primaryKey) {
-        public Field(String jsonPropertyName, String columnName, PgColumn.Type pgType, boolean nullable, boolean queryable) {
-            this(jsonPropertyName, columnName, pgType, nullable, queryable, false);
-        }
-        public PgColumn pgColumn() {
-            return new PgColumn(columnName, pgType, nullable, primaryKey);
-        }
+  /**
+   * Implement to map from request body JSON to entity POJO.
+   *
+   * @param json incoming JSON body
+   * @return Entity POJO
+   */
+  public abstract Entity fromJson(JsonObject json);
 
-        public String pgColumnDdl() {
-            return pgColumn().getColumnDdl();
-        }
-    }
+  /**
+   * Implement to map for entity POJO to response JSON
+   *
+   * @return json representation of the entity
+   */
+  public abstract JsonObject asJson();
 
-    /**
-     * Implement to provide a map of the {@link Field} fields of the implementing entity
-     * @return Map fields by field keys to be used for finding queryable fields or, if possible, for creating the database table and more.
-     */
-    public abstract Map<String, Field> fields();
+  /**
+   * Mapping of metadata to JSON
+   *
+   * @param json target JSON
+   */
+  public void putMetadata(JsonObject json) {
+    json.put("metadata", metadata.asJson());
+  }
 
-    /**
-     * Implement to map from request body JSON to entity POJO.
-     * @param json incoming JSON body
-     * @return Entity POJO
-     */
-    public abstract Entity fromJson(JsonObject json);
+  /**
+   * Mapping of metadata to SQL insert/update templates
+   *
+   * @param parameters template parameters for metadata properties
+   */
+  public void putMetadata(Map<String, Object> parameters) {
+    parameters.putAll(metadata.asTemplateParameters());
+  }
 
-    /**
-     * Implement to map for entity POJO to response JSON
-     * @return json representation of the entity
-     */
-    public abstract JsonObject asJson();
+  /**
+   * Vert.x / Postgres template for table insert, using a tuple mapper.
+   * This base implementation assumes a simple one-to-one mapping of values to columns. It should be
+   * overridden if some entity fields should not be included in the insert statement (virtual fields for example)
+   * or additional hardcoded insert values should be applied or some transformations need to happen to the values
+   * on the fly, like date or time formatting.
+   */
+  public String insertTemplate(String schema) {
+    return "INSERT INTO " + schema + "." + table()
+        + " (" + insertClauseColumns() + ")"
+        + " VALUES (" + insertClauseValueTemplates() + ")";
+  }
 
-    public void putMetadata(JsonObject json) {
-      json.put("metadata", metadata.asJson());
-    }
+  public String updateByIdTemplate(UUID entityId, String schema) {
+    return "UPDATE " + schema + "." + table()
+        + " SET "
+        + updateClauseColumnTemplates()
+        + " WHERE id = '" + entityId.toString() + "'";
+  }
 
-    public void putMetadata(Map<String, Object> parameters) {
-      parameters.putAll(metadata.asTemplateParameters());
-    }
-
-    /**
-     * Vert.x / Postgres template for table insert, using a tuple mapper.
-     * This base implementation assumes a simple one-to-one mapping of values to columns. It should be
-     * overridden if some entity fields should not be included in the insert statement (virtual fields for example)
-     * or additional hardcoded insert values should be applied or some transformations need to happen to the values
-     * on the fly, like date or time formatting.
-     */
-    public String insertTemplate(String schema) {
-        return "INSERT INTO " + schema + "." + table()
-                + " (" + insertClauseColumns() + ")"
-                + " VALUES (" + insertClauseValueTemplates() + ")";
-    }
-
-    public String updateByIdTemplate(UUID entityId, String schema) {
-        return "UPDATE " + schema + "." + table()
-                + " SET "
-                + updateClauseColumnTemplates()
-                + " WHERE id = '" + entityId.toString() + "'";
-    }
-
-    protected String insertClauseColumns() {
-      StringBuilder columnListAsString = new StringBuilder();
-      fields().keySet().forEach(field -> columnListAsString.append(dbColumnName(field)).append(","));
-      return columnListAsString.append(metadata.insertClauseColumns()).toString();
-    }
+  protected String insertClauseColumns() {
+    StringBuilder columnListAsString = new StringBuilder();
+    fields().keySet().forEach(field -> columnListAsString.append(dbColumnName(field)).append(","));
+    return columnListAsString.append(metadata.insertClauseColumns()).toString();
+  }
 
   protected String insertClauseValueTemplates() {
     StringBuilder valueListAsString = new StringBuilder();
@@ -152,138 +169,158 @@ public abstract class Entity {
   }
 
   protected String updateClauseColumnTemplates() {
-      StringBuilder listOfColumnsValues = new StringBuilder();
-      fields().keySet().forEach(field ->
-          listOfColumnsValues.append(dbColumnName(field)).append(" = #{").append(dbColumnName(field)).append("},"));
-      return listOfColumnsValues.append(metadata.updateClauseColumnTemplates()).toString();
-    }
+    StringBuilder listOfColumnsValues = new StringBuilder();
+    fields().keySet().forEach(field ->
+        listOfColumnsValues.append(dbColumnName(field)).append(" = #{").append(dbColumnName(field)).append("},"));
+    return listOfColumnsValues.append(metadata.updateClauseColumnTemplates()).toString();
+  }
 
 
   /**
-     * Creates vert.x row mapper that maps a database select result row onto data object(s).
-     */
-    public abstract RowMapper<Entity> fromRow();
+   * Creates Vert.X row mapper that maps a database select result row onto data object(s).
+   */
+  public abstract RowMapper<Entity> fromRow();
 
-    /**
-     * Creates vert.x tuple mapper that maps Postgres column names to field values.
-     */
-    public abstract TupleMapper<Entity> toTemplateParameters();
+  /**
+   * Adds the record metadata to the Vert.x row mapper.
+   *
+   * @param row The database record to map from
+   */
+  public Entity withMetadata(Row row) {
+    metadata = new Metadata().fromRow(row);
+    return this;
+  }
+
+  /**
+   * Creates vert.x tuple mapper that maps Postgres column names to field values.
+   */
+  public abstract TupleMapper<Entity> toTemplateParameters();
 
 
-    /**
-     * Gets Postgres/CQL definition, containing listing of queryable fields.
-     */
-    public PgCqlDefinition getQueryableFields(){
-        PgCqlDefinition pgCqlDefinition = PgCqlDefinition.create();
-        pgCqlDefinition.addField("cql.allRecords", new PgCqlFieldAlwaysMatches());
-        for (Field entityField : fields().values()) {
-            if (entityField.queryable()) {
-                pgCqlDefinition.addField(entityField.jsonPropertyName(), entityField.pgColumn().pgCqlField());
-            }
-        }
-        return pgCqlDefinition;
+  /**
+   * Gets Postgres/CQL definition, containing listing of queryable fields.
+   */
+  public PgCqlDefinition getQueryableFields() {
+    PgCqlDefinition pgCqlDefinition = PgCqlDefinition.create();
+    pgCqlDefinition.addField("cql.allRecords", new PgCqlFieldAlwaysMatches());
+    for (Field entityField : fields().values()) {
+      if (entityField.queryable()) {
+        pgCqlDefinition.addField(entityField.jsonPropertyName(), entityField.pgColumn().pgCqlField());
+      }
     }
+    return pgCqlDefinition;
+  }
 
-    /**
-     * Map of JSON property names to Postgres table column definitions (PgColumns).
-     */
-    public Map<String, PgColumn> getPropertyToColumnMap() {
-        return fields().values().stream().collect(Collectors.toMap(Field::jsonPropertyName,Field::pgColumn ));
+  /**
+   * Map of JSON property names to Postgres table column definitions (PgColumns).
+   */
+  public Map<String, PgColumn> getPropertyToColumnMap() {
+    return fields().values().stream().collect(Collectors.toMap(Field::jsonPropertyName, Field::pgColumn));
+  }
+
+  /**
+   * Gets a SQL query string from CQL.
+   */
+  public SqlQuery cqlToSql(ServiceRequest request, String schemaDotTable) {
+    PgCqlDefinition definition = getQueryableFields();
+
+    String query = request.requestParam("query");
+    String offset = request.requestParam("offset");
+    String limit = request.requestParam("limit");
+
+    String select = "SELECT * ";
+    String from = "FROM " + schemaDotTable;
+    String whereClause = "";
+    String orderByClause = "";
+    if (query != null && !query.isEmpty()) {
+      PgCqlQuery pgCqlQuery = definition.parse(query);
+      if (pgCqlQuery.getWhereClause() != null) {
+        whereClause = jsonPropertiesToColumnNames(pgCqlQuery.getWhereClause());
+      }
+      if (pgCqlQuery.getOrderByClause() != null) {
+        orderByClause = jsonPropertiesToColumnNames(pgCqlQuery.getOrderByClause());
+      }
     }
+    return new SqlQuery(select, from, whereClause, orderByClause, offset, limit);
+  }
 
-    /**
-     * Gets a SQL query string.
-     */
-    public SqlQuery cqlToSql(ServiceRequest request, String schemaDotTable) {
-        PgCqlDefinition definition = getQueryableFields();
-
-        String query = request.requestParam("query");
-        String offset =  request.requestParam("offset");
-        String limit = request.requestParam("limit");
-
-        String select = "SELECT * ";
-        String from = "FROM " + schemaDotTable;
-        String whereClause = "";
-        String orderByClause = "";
-        if (query != null && !query.isEmpty()) {
-            PgCqlQuery pgCqlQuery = definition.parse(query);
-            if (pgCqlQuery.getWhereClause() != null) {
-                whereClause = jsonPropertiesToColumnNames(pgCqlQuery.getWhereClause());
-            }
-            if (pgCqlQuery.getOrderByClause() != null) {
-                orderByClause = jsonPropertiesToColumnNames(pgCqlQuery.getOrderByClause());
-            }
-        }
-        return new SqlQuery(select, from, whereClause, orderByClause, offset, limit);
+  /**
+   * Crosswalk JSON property names to table column names.
+   *
+   * @param clause string containing names to translate
+   * @return translated string
+   */
+  private String jsonPropertiesToColumnNames(String clause) {
+    if (clause != null) {
+      Map<String, PgColumn> prop2col = getPropertyToColumnMap();
+      for (Map.Entry<String, PgColumn> property : prop2col.entrySet()) {
+        clause = clause.replaceAll(property.getKey().toLowerCase(), prop2col.get(property.getKey()).name);
+      }
     }
+    return clause;
+  }
 
-    /**
-     * Crosswalk JSON property names to table column names.
-     * @param clause string containing names to translate
-     * @return translated string
-     */
-    private String jsonPropertiesToColumnNames(String clause) {
-        if (clause != null) {
-            Map<String, PgColumn> prop2col = getPropertyToColumnMap();
-            for (Map.Entry<String, PgColumn> property : prop2col.entrySet()) {
-                clause = clause.replaceAll(property.getKey().toLowerCase(), prop2col.get(property.getKey()).name);
-            }
-        }
-        return clause;
+  public Field field(String fieldCode) {
+    return fields().get(fieldCode);
+  }
+
+  public String jsonPropertyName(String key) {
+    return field(key).jsonPropertyName();
+  }
+
+  public String dbColumnName(String key) {
+    return field(key).columnName();
+  }
+
+  public String dbColumnType(String key) {
+    return field(key).pgType().name();
+  }
+
+  public String dbColumnNameAndType(String key) {
+    return dbColumnName(key) + " " + dbColumnType(key);
+  }
+
+  /**
+   * For naming the property of a JSON collection response
+   *
+   * @return the JSON property name for a collection of the entity
+   */
+  public abstract String jsonCollectionName();
+
+  /**
+   * For logging statements and response messages.
+   *
+   * @return label to display in messages.
+   */
+  public abstract String entityName();
+
+  public UUID getUuidOrGenerate(String uuidAsString) {
+    try {
+      return UUID.fromString(uuidAsString);
+    } catch (Exception e) {
+      return UUID.randomUUID();
     }
+  }
 
-    public Field field(String fieldCode) {
-        return fields().get(fieldCode);
-    }
+  /**
+   * Setting current user (if any) as the creating user in the record's metadata
+   *
+   * @param currentUser UUID of current user
+   */
+  public Entity withCreatingUser(UUID currentUser) {
+    metadata = new Metadata().withCreatedByUser(currentUser).withCreatedDate(SettableClock.getLocalDateTime().toString());
+    return this;
+  }
 
-    public String jsonPropertyName(String key) {
-        return field(key).jsonPropertyName();
-    }
+  /**
+   * Setting current user (if any) as the updating user in the record's metadata
+   *
+   * @param currentUser UUID of current user
+   */
+  public Entity withUpdatingUser(UUID currentUser) {
+    metadata = new Metadata().withUpdatedByUser(currentUser).withUpdatedDate(SettableClock.getLocalDateTime().toString());
+    return this;
+  }
 
-    public String dbColumnName(String key) {
-        return field(key).columnName();
-    }
-
-    public String dbColumnType(String key) {
-        return field(key).pgType().name();
-    }
-
-    public String dbColumnNameAndType(String key) {
-        return dbColumnName(key) + " " + dbColumnType(key);
-    }
-    /**
-     * For building JSON collection response
-     * @return the JSON property name for a collection of the entity
-     */
-    public abstract String jsonCollectionName();
-
-    /**
-     * For logging and response messages.
-     * @return label to display in messages.
-     */
-    public abstract String entityName();
-
-    public UUID getUuidOrGenerate(String uuidAsString) {
-        try {
-            return UUID.fromString(uuidAsString);
-        } catch (Exception e) {
-            return UUID.randomUUID();
-        }
-    }
-
-    public Entity withCreatingUser(UUID currentUser) {
-      metadata = new Metadata().withCreatedByUser(currentUser).withCreatedDate(SettableClock.getLocalDateTime().toString());
-      return this;
-    }
-
-    public Entity withUpdatingUser(UUID currentUser) {
-      metadata = new Metadata().withUpdatedByUser(currentUser).withUpdatedDate(SettableClock.getLocalDateTime().toString());
-      return this;
-    }
-
-    public Entity withMetadata(Row dbRow) {
-      metadata = new Metadata().fromRow(dbRow);
-      return this;
-    }
 
 }

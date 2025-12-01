fc576a103d4deda6711b2659930d2152cd583b43
nielserik <nielserik@indexdata.com>
2025-12-01 08:14:11 +0100

    Tweak test.

--- src/test/java/org/folio/inventoryupdate/unittests/ImportTests.java
+++ src/test/java/org/folio/inventoryupdate/unittests/ImportTests.java
@@ -777,7 +777,7 @@ public class ImportTests extends InventoryUpdateTestBase {
         getRecordById(Service.PATH_IMPORT_CONFIGS, importConfigId);
         getRecordById(Service.PATH_TRANSFORMATIONS, transformationId);
 
-        Files.filesOfInventoryXmlRecords(5,1000, "200")
+        Files.filesOfInventoryXmlRecords(5,100, "200")
                 .forEach(xml -> postSourceXml(Service.PATH_IMPORT_CONFIGS + "/" + importConfigId + "/import", xml));
 
         await().until(() ->  getTotalRecords(Service.PATH_IMPORT_JOBS), is(1));
@@ -785,8 +785,8 @@ public class ImportTests extends InventoryUpdateTestBase {
         String jobId = getRecords(Service.PATH_IMPORT_JOBS).extract().path("importJobs[0].id");
         String started = getRecordById(Service.PATH_IMPORT_JOBS, jobId).extract().path("started");
         await().until(() -> getRecordById(Service.PATH_IMPORT_JOBS, jobId).extract().path("finished"), greaterThan(started));
-        getRecordById(Service.PATH_IMPORT_JOBS, jobId).body("amountImported", is(5000));
-        assertThat("Instances in storage", fakeFolioApis.instanceStorage.getRecords().size(), is(5000));
+        getRecordById(Service.PATH_IMPORT_JOBS, jobId).body("amountImported", is(500));
+        assertThat("Instances in storage", fakeFolioApis.instanceStorage.getRecords().size(), is(500));
     }
 
     @Test
